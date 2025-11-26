3ebd10372c2d971ffb0488ce6f01a57af64a47b3
santiago tejada <santitejada1122@gmail.com>
2025-11-25 23:35:38 -0300

    Agrego mensajes descriptivos al endpoint /mutant

--- src/main/java/com/example/mutant/controller/MutantController.java
+++ src/main/java/com/example/mutant/controller/MutantController.java
@@ -3,6 +3,7 @@ package com.example.mutant.controller;
 import com.example.mutant.dto.DnaRequest;
 import com.example.mutant.service.MutantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +14,18 @@ public class MutantController {
     private final MutantService mutantService;
 
     @PostMapping("/mutant")
-    public ResponseEntity<Void> isMutant(@RequestBody DnaRequest request) {
-        String[] dnaArray = request.getDna().toArray(new String[0]);
+    public ResponseEntity<String> isMutant(@RequestBody DnaRequest req) {
+        String[] dna = req.getDna().toArray(new String[0]);
+        boolean isMutant = mutantService.processDna(dna);
 
-        boolean isMutant = mutantService.processDna(dnaArray);
-
-        return isMutant
-                ? ResponseEntity.ok().build()
-                : ResponseEntity.status(403).build();
+        if (isMutant) {
+            return ResponseEntity
+                    .ok("El ADN analizado corresponde a un MUTANTE.");
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("El ADN analizado corresponde a un HUMANO (no es mutante).");
+        }
     }
+
 }
